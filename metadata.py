889c8a2a03ba3f7fcda9d02620d07ffe3e1b37e4
jiangjiawei1103 <waynechuang97@gmail.com>
2023-12-19 19:33:45 +0800

    feat: Add more metadata for common access

--- metadata.py
+++ metadata.py
@@ -7,6 +7,7 @@ UNIT_ID_COL = "prediction_unit_id"
 TGT_COL = "target"
 DBI = "data_block_id"
 TGT_PK_COLS = ["county", "is_business", "product_type"]
+REVEALED_TGT_COLS = [UNIT_ID_COL, "datetime", TGT_COL, "is_consumption"]
 COORD_COL2ABBR = {"latitude": "lat", "longitude": "lon"}
 PRODUCT_TYPE2NAME = {0: "Combined", 1: "Fixed", 2: "General service", 3: "Spot"}
 
@@ -14,6 +15,11 @@ PRODUCT_TYPE2NAME = {0: "Combined", 1: "Fixed", 2: "General service", 3: "Spot"}
 CLI_JOIN_KEYS = ["county", "is_business", "product_type", DBI]
 FWTH_JOIN_KEYS = ["county", DBI, "datetime"]  # datetime is the fixed version of forecast_datetime
 REVEALED_TGT_JOIN_KEYS = [UNIT_ID_COL, "datetime", "is_consumption"]
+REVEALED_TGT_ROLLING_JOIN_KEYS = [UNIT_ID_COL, "datetime", "is_consumption"]
+
+# == Groupby keys ==
+REVEALED_TGT_ROLLING_GP_KEYS = [UNIT_ID_COL, "is_consumption"]
 
 # == Temp ==
-FIX_FWTH_TIMEZONE = True
+# Time zone has been fixed in the raw data
+FIX_FWTH_TIMEZONE = False
