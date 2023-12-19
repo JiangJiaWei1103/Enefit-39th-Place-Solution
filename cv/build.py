087b2c640ca0aadd9fd62a24f20ad87ccae0c8f5
jiangjiawei1103 <waynechuang97@gmail.com>
2023-12-19 21:31:52 +0800

    feat: Add PurgedGroupTSSplit

--- cv/build.py
+++ cv/build.py
@@ -30,7 +30,7 @@ def build_cv(**cv_cfg: Any) -> BaseCrossValidator:
     elif cv_scheme == "gpkf":
         cv = GroupKFold(n_folds)
     elif cv_scheme == "tscv":
-        cv = TSCV(n_folds, cv_cfg["val_size"], cv_cfg["n_gap_months"])
+        cv = TSCV(n_folds, cv_cfg["val_size"], cv_cfg["n_gap_months"], cv_cfg["max_train_size"])
 
     return cv
 
@@ -42,33 +42,37 @@ class TSCV:
         n_splits: number of splits
         val_size: number of validation months per fold
         n_gap_months: number of gap months between train and val sets
+        max_train_size: maximal number of training months per fold
     """
 
-    def __init__(self, n_splits: int = 5, val_size: int = 3, n_gap_months: int = 0) -> None:
+    def __init__(
+        self, n_splits: int = 5, val_size: int = 3, n_gap_months: int = 0, max_train_size: Optional[int] = None
+    ) -> None:
         self.n_splits = n_splits
         self.val_size = val_size
         self.n_gap_months = n_gap_months
+        self.max_train_size = max_train_size
 
     def split(
         self, X: pd.DataFrame, y: Optional[pd.Series] = None, groups: Optional[pd.Series] = None
     ) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
-        """Split the data.
-
-        Pass timestamp indicator as pseudo groups.
-        """
         years = groups.dt.year
         months = groups.dt.month
-        # dates = X["year"] * 12 + X["month"].astype(np.int8)
         dates = years * 12 + months
         uniq_dates = sorted(dates.unique().tolist())
 
-        for val_month_end in uniq_dates[-self.n_splits :]:
+        val_months_end = [uniq_dates[-(i * self.val_size + 1)] for i in range(self.n_splits)]
+        for val_month_end in val_months_end:
             val_month_start = val_month_end - self.val_size
             val_mask = (dates > val_month_start) & (dates <= val_month_end)
             val_idx = X[val_mask].index.to_numpy()
 
             tr_month_end = val_month_start - self.n_gap_months
-            tr_mask = dates <= tr_month_end
+            if self.max_train_size is not None:
+                tr_month_start = tr_month_end - self.max_train_size
+                tr_mask = (dates > tr_month_start) & (dates <= tr_month_end)
+            else:
+                tr_mask = dates <= tr_month_end
             tr_idx = X[tr_mask].index.to_numpy()
 
             yield tr_idx, val_idx
